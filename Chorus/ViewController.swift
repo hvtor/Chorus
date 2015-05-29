e750de60f04133d358e9ba0e378269ed5218d121
Hemant Torsekar <hvt@me.com>
2015-05-29 02:11:52 -0400

    Initial Commit

---
+++ Chorus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chorus
+//
+//  Created by Hemant V. Torsekar on 2015-05-29.
+//  Copyright (c) 2015 Hemant V. Torsekar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
